f08f299260d06306ed08f461625c46204788aa14
ogheneovo12 <ukuanovweogheneovo@gmail.com>
2020-10-02 12:05:30 +0100

    fix merge conflict closing #3

--- cache_checker.js
+++ cache_checker.js
@@ -1,22 +1,24 @@
-const puppeteer = require("puppeteer");
-const args = process.argv.slice(2)[0];
+const puppeteer = require('puppeteer');
+const https = require("https");
+const http = require("http");
+// constants
+const URL_HAS_PROTOCOL_REGEX = /^https?:\/\//i;
+
 /*
  *This function loads the webpage first time so i can be cached
  *@note only if website using cache
  *
  */
-first_load = async (URL) => {
+first_load = async (URL,flag) => {
   //Create browser instance
   const browser = await puppeteer.launch({
     userDataDir: "/tmp/user-data-dir", //<--- to save user data like cache
     headless: true, //<-- false for headful mode
     args: ["--no-sandbox"],
   });
-
   const page = await browser.newPage();
-
   const response = await page.goto(URL);
-  if(args == "--server" || args == "--s"  ){
+  if(flag == "--server" || flag == "--s"  ){
     const extendedServerDetails = {domainName:URL,...response.headers(),...response.remoteAddress()};
     console.table(extendedServerDetails);
   }
@@ -30,76 +32,134 @@ first_load = async (URL) => {
  *
  *
  */
-is_cached = async (URL) => {
-  console.log("Cache Checker is running on " + URL);
-  console.log("Please wait.....");
-   await first_load(URL); //<-- This function loads the webpage first time so i can be cached
+is_cached = async (URL,flag) => {
+    console.log("Cache Checker is running on " + URL);
+    console.log("Please wait.....")
 
-  //Create browser instance
-  const browser = await puppeteer.launch({
-    userDataDir: "/tmp/user-data-dir", //<--- to save user data like cache
-    headless: true, //<-- false for headful mode
-    args: ["--no-sandbox"],
-  });
+    const updatedURL = URL.match(URL_HAS_PROTOCOL_REGEX) ? URL : `https://${URL}`;
 
-  const page = await browser.newPage();
+    await first_load(updatedURL,flag); //<-- This function loads the webpage first time so i can be cached 
 
-  // Data Map to store the results
-  let dataHashMap = {
-    css: [],
-    html: [],
-    image: [],
-    js: [],
-    font: [],
-    other: [],
-  };
-  is_leverage_cache = "";
-  page.on("requestfinished", (request) => {
-    let response = request.response();
+    //Create browser instance
+    const browser = await puppeteer.launch({
+        userDataDir: '/tmp/user-data-dir', //<--- to save user data like cache
+        headless: true, //<-- false for headful mode
+        args: ['--no-sandbox'],
+    });
 
-    if (request.resourceType() === "stylesheet") {
-      dataHashMap.css.push([request.url(), response._fromDiskCache]);
-    } else if (request.resourceType() === "script") {
-      dataHashMap.js.push([request.url(), response._fromDiskCache]);
-    } else if (request.resourceType() === "image") {
-      dataHashMap.image.push([request.url(), response._fromDiskCache]);
-    } else if (request.resourceType() === "font") {
-      dataHashMap.font.push([request.url(), response._fromDiskCache]);
-    } else if (request.resourceType() === "document") {
-      let cache = response._fromDiskCache;
+    const page = await browser.newPage();
 
-      if (cache === false) {
-        if (response._status === 304) {
-          //<--The HTTP 304 Not Modified client redirection response code indicates that there is no need to retransmit the requested resources.
-          //It is an implicit redirection to a cached resource.
-          cache = true;
+    // Data Map to store the results
+    let dataHashMap = {
+        'css': [],
+        'html': [],
+        'image': [],
+        'js': [],
+        'font': [],
+        'compression':[],
+        'count': {
+            'cssCount': 0,
+            'imageCount': 0,
+            'jsCount': 0,
+            'fontCount': 0,
+        },
+        'other': []
+    };
+    is_leverage_cache = '';
+    page.on('requestfinished', (request) => {
+
+        let response = request.response();
+
+        //check for compression headers in request
+        if (response.headers()['content-encoding'] !== undefined)
+            dataHashMap.compression.push([
+                request.url(),
+                response.headers()["content-encoding"],
+            ]);
+
+        if (request.resourceType() === 'stylesheet') {
+
+            dataHashMap.css.push([request.url(), response._fromDiskCache]);
+            dataHashMap.count.cssCount++;
+        } else if (request.resourceType() === 'script') {
+
+            dataHashMap.js.push([request.url(), response._fromDiskCache]);
+            dataHashMap.count.jsCount++;
+        } else if (request.resourceType() === 'image') {
+
+            dataHashMap.image.push([request.url(), response._fromDiskCache]);
+            dataHashMap.count.imageCount++;
+        } else if (request.resourceType() === 'font') {
+
+            dataHashMap.font.push([request.url(), response._fromDiskCache]);
+            dataHashMap.count.fontCount++;
+        } else if (request.resourceType() === 'document') {
+
+            let cache = response._fromDiskCache;
+
+
+            if (cache === false) {
+
+                if (response._status === 304) //<--The HTTP 304 Not Modified client redirection response code indicates that there is no need to retransmit the requested resources. 
+                //It is an implicit redirection to a cached resource.
+                {
+                    cache = true;
+                }
+
+            } else {
+                cache = response._fromDiskCache;
+            }
+
+            if(cache===true) { is_leverage_cache= `${URL} is leveraging browser cache`  }else { is_leverage_cache= `${URL} does not leverage browser cache`};
+          
+            dataHashMap.html.push([request.url(), cache]);
+        } else {
+            dataHashMap.other.push([request.url(), response._fromDiskCache]);
         }
-      } else {
-        cache = response._fromDiskCache;
-      }
 
-      if (cache === true) {
-        is_leverage_cache = `${URL} is leveraging broweser cache`;
-      } else {
-        is_leverage_cache = `${URL} does not leverage broweser cache`;
-      }
+    });
 
-      dataHashMap.html.push([request.url(), cache]);
-    } else {
-      dataHashMap.other.push([request.url(), response._fromDiskCache]);
+    await page.goto(updatedURL);
+    await page.waitFor(5000);
+
+    //Print the Results
+    console.log(dataHashMap);
+    console.log(is_leverage_cache);
+    browser.close();
+    console.log('Completed!');
+
+    /* To check if the URL has SSL certificate */
+    const options = URL;
+    var client = http; // To check if the http has a SSL certificate
+    if (URL.indexOf("https") === 0) {
+        client = https;
     }
-  });
+    var req = client.request(options, function (res) {
+        if (res.socket.authorized) {
+            console.log("The website uses SSL Certificate!!");
+        } else {
+            console.log("The website does not use SSL Certificate!!");
+        }
+    });
+    req.on('error', (e) => {
+        console.error(e);
+    })
+    req.end();
+}
 
-  await page.goto(URL);
-  await page.waitFor(5000);
-
-  //Print the Results
-  console.log(dataHashMap);
-  console.log(is_leverage_cache);
-  browser.close();
-  console.log("Completed!");
-};
 
 //is_cached('https://github.com');
 
-is_cached("https://ilmtechnosolutions.com");
+// process command line argument
+const [url,flag] = process.argv.slice(2);
+if (!url) {
+    exitScript("please enter a valid link")
+} else {
+    if(url.startsWith("--")) exitScript("please enter a valid link");
+    is_cached(url,flag);
+}
+
+function exitScript(message){
+    console.log(message);
+    process.exit(1);
+}
